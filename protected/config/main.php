fd4f9004bf29e6b23c7b6cd265d180f7e64e21a1
XFHNever <443696948@qq.com>
2013-12-07 20:10:25 +0800

    yii-bootstrap配置成功，解决一个两者结合的冲突

--- protected/config/main.php
+++ protected/config/main.php
@@ -7,8 +7,10 @@
 // CWebApplication properties can be configured here.
 return array(
 	'basePath'=>dirname(__FILE__).DIRECTORY_SEPARATOR.'..',
-	'name'=>'My Web Application',
-
+	'name'=>'Never Blog',
+        'theme'=>'bootstrap',
+         
+        'defaultController'=>'',
 	// preloading 'log' component
 	'preload'=>array('log'),
 
@@ -16,18 +18,22 @@ return array(
 	'import'=>array(
 		'application.models.*',
 		'application.components.*',
+                'ext.bootstrap-theme.widgets.*',
+                'ext.bootstrap-theme.helpers.*',
+                'ext.bootstrap-theme.behaviors.*',
 	),
 
 	'modules'=>array(
 		// uncomment the following to enable the Gii tool
-		/*
+		/**/
 		'gii'=>array(
 			'class'=>'system.gii.GiiModule',
-			'password'=>'Enter Your Password Here',
+			'password'=>'xfhlz920805',
 			// If removed, Gii defaults to localhost only. Edit carefully to taste.
 			'ipFilters'=>array('127.0.0.1','::1'),
+                        'generatorPaths'=>array('ext.bootstrap-theme.gii',),
 		),
-		*/
+		
 	),
 
 	// application components
@@ -36,6 +42,9 @@ return array(
 			// enable cookie-based authentication
 			'allowAutoLogin'=>true,
 		),
+                'bootstrap'=>array(//添加一个新的bootstrap容器
+                   'class'=>'bootstrap.components.Bootstrap', //执行Bootstrap.php文件
+                ),
 		// uncomment the following to enable URLs in path-format
 		/*
 		'urlManager'=>array(
@@ -48,7 +57,11 @@ return array(
 		),
 		*/
 		'db'=>array(
-			'connectionString' => 'sqlite:'.dirname(__FILE__).'/../data/testdrive.db',
+			'connectionString' => 'mysql:host=localhost;dbname=myblog',
+			'emulatePrepare' => true,
+			'username' => 'root',
+			'password' => 'root',
+			'charset' => 'utf8',
 		),
 		// uncomment the following to use a MySQL database
 		/*
